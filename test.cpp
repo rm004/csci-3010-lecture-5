ad90a997c8c5b4d1e97b39666630ab746364f04b
rm004 <rmckiel002@gmail.com>
2025-01-28 15:14:38 -0700

    feat: added second test case for AddN

--- test.cpp
+++ test.cpp
@@ -18,4 +18,11 @@ TEST_CASE("Double is added to each element", "AddN") {
 	for (int i = 0; i < (int)test1.size(); i++) {
 		REQUIRE(test1[i] == correct1[i]);
 	}
+
+	std::vector<double> vec2 = {1, 2, 3, 4};
+	std::vector<double> test2 = AddN(vec2, -1.0);
+	std::vector<double> correct2 = {0, 1, 2, 3};
+	for (int i = 0; i < (int)test2.size(); i++) {
+		REQUIRE(test2[i] == correct2[i]);
+	}
 }
